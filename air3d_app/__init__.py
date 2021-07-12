5383d815f05489a7b359fc9b810a8bdf7a1ebafe
Kou-kun42 <shinigamithekido@gmail.com>
2021-07-11 21:40:55 -0400

    Added file uploads

--- air3d_app/__init__.py
+++ air3d_app/__init__.py
@@ -9,16 +9,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = os.urandom(24)
 
+# Limits file upload size to 16mb
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+# Sets config properties for flask uploads
+OFFERS_FOLDER = './uploads/offers'
+app.config["UPLOADED_OFFERS_DEST"] = OFFERS_FOLDER
+REQUESTS_FOLDER = './uploads/requests'
+app.config["UPLOADED_REQUESTS_DEST"] = REQUESTS_FOLDER
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-
-# from air3d_app.main.routes import main
-# from air3d_app.auth.routes import auth
-# from air3d_app.auth.tests import tests
-# app.register_blueprint(main)
-# app.register_blueprint(auth)
-# app.register_blueprint(tests)
 
 from air3d_app.main.routes import main as main_routes
 app.register_blueprint(main_routes)
--- air3d_app/main/routes.py
+++ air3d_app/main/routes.py
@@ -7,8 +7,11 @@ from flask import (
     session,
     flash
 )
+from werkzeug.utils import secure_filename
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 from bson.objectid import ObjectId
 import requests
+import os
 from air3d_app import app, db
 from flask_login import login_user, logout_user, login_required, current_user
 from datetime import date, datetime
@@ -16,8 +19,12 @@ from air3d_app.models import User, Profile, Requests, Design
 from air3d_app.main.forms import ProfileForm, RequestForm, DesignForm
 from air3d_app import bcrypt
 
-
 main = Blueprint('main', __name__)
+
+# Create Upload sets for offers and design requests
+offers = UploadSet("offers", IMAGES)
+design_requests = UploadSet("requests", IMAGES)
+configure_uploads(app, (offers, design_requests))
 
 
 # Homepage
@@ -41,15 +48,31 @@ def product_requests():
     return render_template('product-requests.html')
 
 
+# Check to make sure the upload type is allowed
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
+
+
 # Order Form
-@main.route('/order-form')
+@main.route('/order-form', methods=['GET', 'POST'])
 def order_form():
     '''Display order form'''
+    # Handle file upload
+    if request.method == 'POST' and 'request' in request.files:
+        design_requests.save(request.files['request'])
+        flash("Request submitted")
+        return render_template('order-form.html')
     return render_template('order-form.html')
 
 
-# Product Offers
+# Product Offers Upload Form
 @main.route('/design-upload-form')
 def design_upload_form():
     '''Display design upload form'''
+    # Handle file upload
+    if request.method == 'POST' and 'offer' in request.files:
+        offers.save(request.files['offer'])
+        flash("Offer submitted")
+        return render_template('design-upload-form.html')
     return render_template('design-upload-form.html')
