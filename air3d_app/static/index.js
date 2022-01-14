367c53d4964e06b6c8f9f05b21493023bc16e80b
Kou-kun42 <shinigamithekido@gmail.com>
2022-01-14 14:41:43 -0500

    Changed from http to https

--- air3d_app/static/index.js
+++ air3d_app/static/index.js
@@ -1,5 +1,5 @@
 // Socket Logic for Chat
-var socket = io.connect("http://" + document.domain + ":" + location.port);
+var socket = io.connect("https://" + document.domain + ":" + location.port);
 
 socket.on("connect", function () {
     socket.emit("message", {
